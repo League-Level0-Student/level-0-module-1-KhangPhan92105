5832241c6d3ab0d4d92982154894157125399f56
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-11-07 13:28:21 -0800

    eyebrows

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -6,7 +6,20 @@ import javax.swing.JOptionPane;
 
 public class Remarkable {
 public static void main(String[] args) {
-
+	
+	String username = JOptionPane.showInputDialog("What is your name?");
+	String aboutKhang ="You are very tall.";
+	String aboutAnthony= "You are good at basketball.";
+	String aboutNhan= " Big Brain.";
+	if(username.equalsIgnoreCase("Khang you are very tall"));{
+	if(aboutAnthony.equals("You are good at basketball.")) {
+		if(aboutNhan.equals(" You are big brain.")) {
+			
+		
+			
+		
+	
+	
 		// 1. Save something remarkable about each person in a variable.
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
@@ -15,4 +28,7 @@ public static void main(String[] args) {
 
 	}
 }
+}
 
+}
+}
